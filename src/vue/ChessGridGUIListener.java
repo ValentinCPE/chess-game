eb8530bb90710f2e98b16825d0f932e7b4484c39
Stephane CONQ <conqstephane@gmail.com>
2018-06-11 09:22:10 +0200

    (sco) fin config go bosser laaaaaaa

--- src/vue/ChessGridGUIListener.java
+++ src/vue/ChessGridGUIListener.java
@@ -1,14 +1,20 @@
 package vue;
 
+import controler.ChessGameControlerModel;
 import controler.ChessGameControlerModelVue;
 import tools.data.Coord;
 import tools.data.Couleur;
 
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class ChessGridGUIListener extends ChessGridGUI implements ChessGameControlerModelVue{
+public class ChessGridGUIListener extends ChessGridGUI implements ChessGameControlerModelVue, MouseListener, MouseMotionListener{
 
+    public ChessGridGUIListener(ChessGridGUI chessGridGUI, ChessGameControlerModelVue chessGameControlerModelVue) {
 
+    }
 
     @Override
     public void setGridPanel(JLayeredPane panel) {
@@ -30,4 +36,39 @@ public class ChessGridGUIListener extends ChessGridGUI implements ChessGameContr
 
     }
 
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
 }
